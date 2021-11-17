e1c9253525942b139434d0d0a5eb221a9dc64094
Elias <neossuribe@gmail.com>
2021-11-17 00:39:46 -0300

    Sección "nosotros" terminada

--- src/app/contenedor/paginas/nosotros/nosotros.module.ts
+++ src/app/contenedor/paginas/nosotros/nosotros.module.ts
@@ -5,9 +5,15 @@ import { NosotrosRoutingModule } from './nosotros-routing.module';
 import { NosotrosComponent } from './nosotros.component';
 
 import { HeroSeccionModule } from 'src/app/shared/components/hero-seccion/hero-seccion.module';
+import { PrimengModule } from 'src/app/primeng-module';
 
 @NgModule({
   declarations: [NosotrosComponent],
-  imports: [CommonModule, NosotrosRoutingModule, HeroSeccionModule],
+  imports: [
+    CommonModule,
+    NosotrosRoutingModule,
+    HeroSeccionModule,
+    PrimengModule,
+  ],
 })
 export class NosotrosModule {}
